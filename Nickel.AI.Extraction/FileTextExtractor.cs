fdf0a3518ac66d251f1d6dcf04fb699bedc3c2c5
Bill Nickel <bill.nickel@gmail.com>
2024-06-06 12:19:20 -0700

    Add naive implementation of parsing .txt files into ExtractedDocument.

--- Nickel.AI.Extraction/FileTextExtractor.cs
+++ Nickel.AI.Extraction/FileTextExtractor.cs
@@ -1,4 +1,5 @@
-﻿using Toxy;
+﻿using System.Text;
+using Toxy;
 
 namespace Nickel.AI.Extraction
 {
@@ -11,11 +12,53 @@ namespace Nickel.AI.Extraction
 
         private ExtractedDocument Extract(string filePath)
         {
+            var extension = Path.GetExtension(filePath);
+
+            if (extension != null && extension.ToLower() == ".txt")
+            {
+                return ExtractFromPlainText(filePath);
+            }
+
             var documentParser = ParserFactory.CreateDocument(new ParserContext(filePath));
             var document = documentParser.Parse();
 
             return document.ToExtractedDocument();
         }
 
+        // use a naive approach for .txt files
+        private ExtractedDocument ExtractFromPlainText(string filePath)
+        {
+            var document = new ExtractedDocument();
+            var buffer = new StringBuilder();
+
+            using StreamReader reader = new StreamReader(filePath);
+
+            var line = reader.ReadLine();
+
+            while (line != null)
+            {
+                if (line.Trim().Length == 0)
+                {
+                    if (buffer.Length > 0)
+                    {
+                        document.Paragraphs.Add(buffer.ToString());
+                        buffer.Clear();
+                    }
+                }
+                else
+                {
+                    buffer.AppendLine(line);
+                }
+
+                line = reader.ReadLine();
+            }
+
+            if (buffer.Length > 0)
+            {
+                document.Paragraphs.Add(buffer.ToString());
+            }
+
+            return document;
+        }
     }
 }

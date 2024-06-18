a5aa709df31481c4ea69fcb142825db535a311b2
Bill Nickel <bill.nickel@gmail.com>
2024-06-18 08:59:44 -0700

    Add SemanticKernel text chunker.

---
+++ Nickel.AI/Chunking/SemanticKernelTextChunker.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable SKEXP0050 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+using Microsoft.SemanticKernel.Text;
+using Nickel.AI.Tokenization;
+
+namespace Nickel.AI.Chunking
+{
+    public class SemanticKernelTextChunker : IChunker
+    {
+        private int _targetTokenCount = 0;
+
+        public SemanticKernelTextChunker(int targetTokenCount)
+        {
+            _targetTokenCount = targetTokenCount;
+        }
+
+        public string[] GetChunks(IBasicTokenizer tokenizer, string text)
+        {
+            int TokenCounter(string text)
+            {
+                return tokenizer.Encode(text).Length;
+            }
+
+            return [.. TextChunker.SplitPlainTextLines(text, _targetTokenCount, TokenCounter)];
+        }
+    }
+}
